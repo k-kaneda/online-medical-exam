ff7c0a11bb480e73acb4f38463477c129612f3b7
k-kaneda <bodily.functions@gmail.com>
2024-10-04 22:54:33 +0900

    fix loginpage's UI

--- app/login/page.js
+++ app/login/page.js
@@ -11,10 +11,12 @@ export default function Login() {
     <div>
       <Header />
       <Flex height='auto' alignItems='center' justifyContent='center' m={16}>
+        <Flex direction='column' padding={12} rounded={6}>
           <Box>
             <Icon as={IoIosWarning} boxSize={24} mr={2} color='blue.100' />
             <Text fontSize={16}>当ページは架空のクリニックのオンライン診療システムです。</Text>
           </Box>
+        </Flex>
         <Flex direction='column' background='gray.100' padding={12} rounded={6}>
           <Heading textAlign='center'>ログイン</Heading>
           <Input m={2} placeholder='sample@sample.com' variant='filled' type='email' />
